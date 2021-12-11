b9bd3bd01b347bd1d61c32b6a26fd606739424db
Praddby <pradd.by@gmail.com>
2021-12-11 20:41:34 +0300

    fix: fix swagger to ts

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
 import fastify from 'fastify';
 import autoLoad from 'fastify-autoload';
 import { join } from 'path';
-// import * as swagger from 'fastify-swagger';
+import SwaggerPlugin from 'fastify-swagger';
 
 const app = fastify({
   logger: true,
 });
 
-// app.register(swagger, {
-//   exposeRoute: true,
-//   routePrefix: '/docs',
-//   swagger: {
-//     info: { title: 'REST service API' },
-//   },
-// });
+app.register(SwaggerPlugin, {
+  exposeRoute: true,
+  routePrefix: '/docs',
+  swagger: {
+    info: { 
+      title: 'REST service API',
+      version: '0.2.0',
+    },
+  },
+});
 
 app.register(autoLoad, {
   dir: join(__dirname, 'routes'),
